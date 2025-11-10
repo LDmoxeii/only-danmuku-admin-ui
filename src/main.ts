f1be622c6c70548a0266f9e1bb243b172077b145
zhenyu.jiang <jiangzhenyu@keyfil.com>
2025-11-10 16:08:52 +0800

    feat: 注册全局工具与常量
    - main.ts 注入 Request/Message/Utils/Verify/Confirm/Api 及常量
    - 新增类型声明 src/types/global.d.ts 适配 TS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './assets/base.scss'
 import './assets/icon/iconfont.css'
+import VueCookies from 'vue-cookies'
+import Request from './utils/request'
+import Message from './utils/message'
+import Utils from './utils/utils'
+import Verify from './utils/verify'
+import Confirm from './utils/confirm'
+import { Api } from './utils/api'
 import './style.css'
 import App from './App.vue'
 import router from './router'
@@ -12,4 +19,20 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
+app.config.globalProperties.VueCookies = VueCookies as any
+app.config.globalProperties.Request = Request as any
+app.config.globalProperties.Message = Message as any
+app.config.globalProperties.Utils = Utils as any
+app.config.globalProperties.Verify = Verify as any
+app.config.globalProperties.Confirm = Confirm as any
+app.config.globalProperties.Api = Api as any
+app.config.globalProperties.bodyMaxWidth = 2000
+app.config.globalProperties.bodyMinWidth = 1080
+app.config.globalProperties.rowCategoryCount = 15
+app.config.globalProperties.chunkSize = 1024 * 512
+app.config.globalProperties.maxUploading = 3
+app.config.globalProperties.videoAccept = '.mp4,.avi,.rmvb,.mkv,.mov'
+app.config.globalProperties.imageAccept = '.jpg,.png,.gif,.bmp,.webp'
+app.config.globalProperties.imageThumbnailSuffix = '_thumbnail.jpg'
+app.config.globalProperties.webDomain = 'http://localhost:3000'
 app.mount('#app')
---
+++ src/types/global.d.ts
@@ -0,0 +1,25 @@
+import 'vue'
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    VueCookies: any
+    Request: any
+    Message: any
+    Utils: any
+    Verify: any
+    Confirm: any
+    Api: any
+    bodyMaxWidth?: number
+    bodyMinWidth?: number
+    rowCategoryCount?: number
+    chunkSize?: number
+    maxUploading?: number
+    videoAccept?: string
+    imageAccept?: string
+    imageThumbnailSuffix?: string
+    webDomain?: string
+  }
+}
+
+export {}
+
