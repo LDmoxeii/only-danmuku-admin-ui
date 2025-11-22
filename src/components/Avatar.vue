b3fb17355bdf05ba58271a23e95ee05f83d467c4
zhenyu.jiang <jiangzhenyu@keyfil.com>
2025-11-22 15:57:42 +0800

    feat(components): 优化 Avatar 组件支持懒加载和默认值
    - 添加 lazy 属性以支持图片懒加载
    - 设置 width 和 lazy 的默认值
    - 更新 Cover 组件调用传递新属性
    - 支持 userId 类型为字符串或数字
    - 引入 withDefaults 方法处理属性默认值

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="image-panel" :style="{ width: width + 'px', height: width + 'px' }">
     <router-link v-if="userId" :to="`/user/${userId}`" target="_blank">
-      <Cover :source="avatar" :defaultImg="defaultAvatar" borderRadius="50%" />
+      <Cover :lazy="lazy" :width="width" :source="avatar" :defaultImg="defaultAvatar" borderRadius="50%" :scale="1" />
     </router-link>
-    <Cover v-else :source="avatar" :defaultImg="defaultAvatar" borderRadius="50%" />
+    <Cover v-else :lazy="lazy" :width="width" :source="avatar" :defaultImg="defaultAvatar" borderRadius="50%" :scale="1" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, withDefaults } from 'vue'
 import Cover from '@/components/Cover.vue'
 
 const defaultAvatar = ref('user.png')
-defineProps<{ width?: number; avatar?: string; userId?: string }>()
+withDefaults(
+  defineProps<{
+    width?: number
+    avatar?: string
+    userId?: string | number
+    lazy?: boolean
+  }>(),
+  {
+    width: 50,
+    lazy: true
+  }
+)
 </script>
 
 <style lang="scss" scoped>
